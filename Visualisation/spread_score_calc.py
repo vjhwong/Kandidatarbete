d07f3b3c45406dca5b53e6ede0c6351671b4f9aa
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-14 11:49:54 +0200

    removed and moved files

--- Visualisation/spread_score_calc.py
+++ Visualisation/spread_score_calc.py
@@ -2,106 +2,12 @@ import pandas as pd
 import numpy as np
 import json
 from pprint import pprint
-
-
-def extract_chosen_isolates(
-    chosen_isolates: pd.DataFrame, matrix_EU: pd.DataFrame
-) -> pd.DataFrame:
-    """
-    Select the chosen isolates from the script. Return a DataFrame only containing the rows of selected isolates
-    """
-    chosen_rows = matrix_EU["Isolate"].isin(chosen_isolates["Isolate"])
-    return matrix_EU[chosen_rows]
-
-
-def find_digits(SIR: str) -> int:
-    """Find numbers in a string"""
-    digit = ""
-    for character in SIR:
-        if character.isdigit() or character == ".":
-            digit += character
-    return float(digit)
-
-
-def get_scale(SIR: str) -> bool:
-    """Get on or off scale. True == on-scale"""
-    if "=" in SIR:
-        return True
-    elif "<" in SIR or ">" in SIR:
-        return False
-    else:
-        raise ValueError("Not a valid SIR")
-
-
-def parse_SIR(SIR: str) -> bool:
-    """
-    Find the isolates with valid SIRs. Not 'Missing BP'
-    and not 'nip'.
-    """
-    if SIR.startswith("Missing BP"):
-        return False
-    if SIR == "nip":
-        return False
-    return True
-
-
-def extract_SIR(chosen_isolates: pd.DataFrame, antibiotics: list) -> dict:
-    """
-    Extract all SIRs for an antibiotic. Returns a dictionary
-    with antibiotcs as keys and lists of the isolates and their
-    SIRs in tuples as value.
-    """
-    chosen_isolates_SIR = {antibiotic: [] for antibiotic in antibiotics}
-
-    for index, row in chosen_isolates.iterrows():
-        isolate, pathogen, antibiotic_SIR = row[0], row[1], list(row[3:].items())
-        for antibiotic, SIR in antibiotic_SIR:
-            if parse_SIR(SIR):
-                mic_category = SIR[0]
-                mic = find_digits(SIR)
-                scale = get_scale(SIR)
-                chosen_isolates_SIR[antibiotic].append(
-                    (isolate, mic, mic_category, scale, pathogen)
-                )
-            else:
-                # If SIR = "Missing BP" or "nip"
-                chosen_isolates_SIR[antibiotic].append(
-                    (isolate, SIR, None, None, pathogen)
-                )
-    return chosen_isolates_SIR
-
-
-def filter_mic_values(chosen_isolates_SIR: dict) -> None:
-    """
-    Remove the tuples that have None in their SIR data
-    """
-    for antibiotic, SIR_data in chosen_isolates_SIR.items():
-        # tup = (isolate, mic_value, mic_category, scale, pathogen)
-        chosen_isolates_SIR[antibiotic] = list(
-            (tup for tup in SIR_data if tup[2] is not None)
-        )
-    return chosen_isolates_SIR
-
-
-def extract_mic_data(chosen_isolates_SIR: dict, antibiotics: list) -> list:
-    """
-    Extract the mic-values of each isolate for each antibiotic.
-    Returns a nested list. Each list represents the mic-values of
-    all isolates for an antibiotic.
-    """
-    mic_values = []
-    # Iterate over all antibioticsz
-    for antibiotic in antibiotics:
-        # Create a list to hold the mic-values of isolates for that abx
-        antibiotic_mic_values = []
-        # Get value of current abx. List of (isolate, mic_value, mic_category)
-        SIR_data = chosen_isolates_SIR[antibiotic]
-        for isolate, mic_value, mic_category, scale, pathogen in SIR_data:
-            antibiotic_mic_values.append(
-                (isolate, np.log2(mic_value), mic_category, scale, pathogen)
-            )
-        mic_values.append(antibiotic_mic_values)
-    return mic_values
+from data_extraction_functions import (
+    extract_chosen_isolates,
+    extract_mic_data,
+    extract_SIR,
+    filter_mic_values,
+)
 
 
 def create_spread_dict(
@@ -138,6 +44,48 @@ def create_spread_dict(
                 elif index > upper_limit_index:
                     spread[index] = None
     return spread_dict
+
+
+def count_gap_length(valid_list: list) -> float:
+    total_gap_length = 0
+    gap_length = 0
+
+    for value in valid_list:
+        if value == 0:
+            gap_length += 1
+        else:  # if current value == 1
+            if gap_length >= 2:
+                total_gap_length += gap_length - 1
+            gap_length = 0
+    # If the last values were also 0s and the gap length is at least 2.
+    # Add the length of the gap to the total gap length
+    if gap_length >= 2:
+        total_gap_length += gap_length - 1
+    return total_gap_length
+
+
+def check_edges(valid_list: list) -> float:
+    edge_penalty = 0
+
+    if valid_list[0] == 0:
+        edge_penalty += 0.5
+    if valid_list[-1] == 0:
+        edge_penalty += 0.5
+
+    return edge_penalty
+
+
+def score_fill_list(fill_list: list) -> float:
+    valid_list = [i for i in fill_list if i is not None]
+
+    if len(valid_list) <= 1:
+        raise ValueError("Length of valid list must be greater than 1")
+
+    penalty = 0
+    penalty += count_gap_length(valid_list)
+    penalty += check_edges(valid_list)
+
+    return penalty
 
 
 def main():
@@ -178,7 +126,7 @@ def main():
     ]
 
     spread_dict = create_spread_dict(antibiotics_ranges, total_concentration_range)
-    print(spread_dict)
+
     # # Select isolates
     # chosen_isolates = extract_chosen_isolates(chosen_isolates_list, matrix_EU)
 

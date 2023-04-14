18cf751bd9d0164b842b58ace6444fdd157c7675
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-14 11:42:48 +0200

    Created create_spread_dict() function that creates a dictionary where the keys are the antibiotic names and the values are lists that will contain a representation of the spread of MIC values for each antibiotic its key (antibiotic). None values mean that the concentration value is off-scale for that antibiotic. 0 means that the antibiotic does not have a MIC value at that concentration which all of them will have since the list is being initialized.

--- Visualisation/spread_score_calc.py
+++ Visualisation/spread_score_calc.py
@@ -104,12 +104,44 @@ def extract_mic_data(chosen_isolates_SIR: dict, antibiotics: list) -> list:
     return mic_values
 
 
+def create_spread_dict(
+    antibiotic_ranges: dict, total_concentration_range: list
+) -> dict:
+
+    # Dictionary that will contain the spread of each antibiotic
+    spread_dict = {
+        antibiotic: [0 for _ in range(len(total_concentration_range))]
+        for antibiotic in antibiotic_ranges
+    }
+
+    # Dictionary to go between concentration and indices
+    concentration_to_index_convert = {
+        concentration: index
+        for index, concentration in enumerate(total_concentration_range)
+    }
+
+    for (_, ranges), (_, spread) in zip(antibiotic_ranges.items(), spread_dict.items()):
+        lower_limit, upper_limit = ranges["Lower"], ranges["Upper"]
+        if lower_limit == "Min_C" and upper_limit == "Max_C":
+            pass
+        else:
+            lower_limit_index = concentration_to_index_convert[lower_limit]
+            upper_limit_index = concentration_to_index_convert[upper_limit]
+            for index in range(len(spread)):
+                if index < lower_limit_index:
+                    spread[index] = None
+                elif index > upper_limit_index:
+                    spread[index] = None
+    return spread_dict
+
+
 def main():
     # Load files
     chosen_isolates_list = pd.read_csv("Visualisation/Chosen_isolates_list.csv")
     CIB = pd.ExcelFile("Visualisation/CIB_TF-data_AllIsolates_20230302.xlsx")
     matrix_EU = pd.read_excel(CIB, "matrix EU")
-    antibiotics_ranges = json.load(open("abx_ranges.json"))
+    antibiotics_ranges = json.load(open("Visualisation/abx_ranges.json"))
+
     # Rename a long name for plotting purposes
     matrix_EU.rename(
         columns={"Trimethoprim-sulfamethoxazole": "Trimeth-sulf"}, inplace=True
@@ -140,21 +172,22 @@ def main():
         "Max C",
     ]
 
-    # Select isolates
-    chosen_isolates = extract_chosen_isolates(chosen_isolates_list, matrix_EU)
+    spread_dict = create_spread_dict(antibiotics_ranges, total_concentration_range)
+    print(spread_dict)
+    # # Select isolates
+    # chosen_isolates = extract_chosen_isolates(chosen_isolates_list, matrix_EU)
 
-    # List of antiiotic names
-    antibiotics = list(chosen_isolates.columns[3:])
+    # # List of antiiotic names
+    # antibiotics = list(chosen_isolates.columns[3:])
 
-    # Extract all SIRs for an antibiotic.
-    chosen_isolates_SIR = extract_SIR(chosen_isolates, antibiotics)
+    # # Extract all SIRs for an antibiotic.
+    # chosen_isolates_SIR = extract_SIR(chosen_isolates, antibiotics)
 
-    # Remove the tuples that have None in their SIR data
-    filtered_chosen_isolates_SIR = filter_mic_values(chosen_isolates_SIR)
-    pprint(filtered_chosen_isolates_SIR)
+    # # Remove the tuples that have None in their SIR data
+    # filtered_chosen_isolates_SIR = filter_mic_values(chosen_isolates_SIR)
 
-    # Extract the mic-values of each isolate for each antibiotic.
-    mic_data = extract_mic_data(filtered_chosen_isolates_SIR, antibiotics)
+    # # Extract the mic-values of each isolate for each antibiotic.
+    # mic_data = extract_mic_data(filtered_chosen_isolates_SIR, antibiotics)
 
 
 if __name__ == "__main__":

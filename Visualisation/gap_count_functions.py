7c300fcbac25b2b73ac3174497f1596190298e4c
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-14 11:52:16 +0200

    created file to store functions used for counting sgaps in spread lists

---
+++ Visualisation/gap_count_functions.py
@@ -0,0 +1,40 @@
+def count_gap_length(valid_list: list) -> float:
+    total_gap_length = 0
+    gap_length = 0
+
+    for value in valid_list:
+        if value == 0:
+            gap_length += 1
+        else:  # if current value == 1
+            if gap_length >= 2:
+                total_gap_length += gap_length - 1
+            gap_length = 0
+    # If the last values were also 0s and the gap length is at least 2.
+    # Add the length of the gap to the total gap length
+    if gap_length >= 2:
+        total_gap_length += gap_length - 1
+    return total_gap_length
+
+
+def check_edges(valid_list: list) -> float:
+    edge_penalty = 0
+
+    if valid_list[0] == 0:
+        edge_penalty += 0.5
+    if valid_list[-1] == 0:
+        edge_penalty += 0.5
+
+    return edge_penalty
+
+
+def score_fill_list(fill_list: list) -> float:
+    valid_list = [i for i in fill_list if i is not None]
+
+    if len(valid_list) <= 1:
+        raise ValueError("Length of valid list must be greater than 1")
+
+    penalty = 0
+    penalty += count_gap_length(valid_list)
+    penalty += check_edges(valid_list)
+
+    return penalty
